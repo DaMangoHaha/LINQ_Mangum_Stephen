ed633b31eb45d11c3fab31e1aee0b4ca1d07a459
Stephen Mangum <stephenmangum01@gmail.com>
2025-09-16 08:26:27 -0500

    Add Game class and LINQ queries to Program
    Introduced a new Game class to represent video games with title, ESRB rating, and genre. Updated Program.cs to create an array of Game objects and demonstrate LINQ queries for filtering and selecting game data.

---
+++ LINQ_Mangum_Stephen/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Mangum_Stephen
+{
+    internal class Game
+    {
+
+
+        public string Title { get; set; }
+
+        public char Esrb { get; set; }
+
+        public string Genre { get; set; }
+
+        public Game(string titleParam, char esrbParam, string genreParam)
+        {
+            Title = titleParam;
+            Esrb = esrbParam;
+            Genre = genreParam;
+        }
+    }
+}
--- LINQ_Mangum_Stephen/Program.cs
+++ LINQ_Mangum_Stephen/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace LINQ_Mangum_Stephen
 {
+    using System.Linq;
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game[] games = new Game[]
+            {
+                new Game("Minecraft", 'E', "Action-Adventure"),
+                new Game("CS:GO", 'M', "FPS"),
+                new Game("Elden Ring", 'M', "Action-Adventure"),
+                new Game("Valorant", 'T', "FPS"),
+                new Game("Halo 3", 'M', "Action FPS"),
+                new Game("ZombCube", 'E', "FPS Survival"),
+                new Game("Magnet Destroyer", 'E', "Hyper-Casual"),
+                new Game("Paddle Balls", 'E', "Arcade Casual"),
+                new Game("Rocket League", 'E', "Action Racing"),
+                new Game("Fifa 22", 'E', "Sport")
+            };
+
+            var shortGames = from game in games
+                             where game.Title.Length < 9
+                             select $"Game Title: {game.Title.ToUpper()}";
+
+            foreach(var game in shortGames)
+            {
+                               Console.WriteLine(game);
+            }
+
+            var mineCraft = games.Where(game => game.Title == "Minecraft")
+                                               .Select(game => $"Game Title: {game.Title}, ESRB Rating: {game.Esrb}, Genre: {game.Genre}");
+
+
+            Console.WriteLine(mineCraft.FirstOrDefault());
         }
     }
 }
